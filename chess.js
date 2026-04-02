ca98a300493d1e009ec531f17b90f305da000b06
MattB <doofymatt@gmail.com>
2026-04-02 10:09:49 -0230

    Added table building method in js and messed with the css

---
+++ chess.js
@@ -0,0 +1,16 @@
+var table = document.createElement("table");
+for (var i = 1; i < 9; i++) {
+    var tr = document.createElement("tr");
+    for (var j = 1; j < 9; j++) {
+        var td = document.createElement("td");
+        if (i%2 == j%2) {
+            td.className = "white";
+        } else {
+            td.classname = "black";
+        }
+        tr.appendChaild(td);
+    }
+    table.appendChild(tr);
+
+}
+document.body.appendChild(table);
